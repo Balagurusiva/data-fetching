931d1a5e41238a8c5780def89a9dfe0dc709cb0b
Balaguru <balagurusiva2002@gmail.com>
2024-05-03 15:12:22 +0530

    modified:   nuxt.config.ts 	deleted:    pages/geo.vue 	deleted:    pages/isr.vue 	deleted:    pages/ssg.vue 	deleted:    pages/ssr.vue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,5 @@
 export default defineNuxtConfig({
   devtools: { enabled: false},
   modules: ["@nuxt/ui", "@nuxt/image"],
-  routeRules:{
-    '/': {ssr:false}, //client side rendering
-    '/ssr':{ssr:true}, // server side rendering
-    '/isr':{isr:2300}, //increamental static rendering
-    '/ssg':{prerender:true}
-  }
+  ssr: false
 })
