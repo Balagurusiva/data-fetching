7999752e1debaefb98d5d4daf7083741c244b912
Balaguru <balagurusiva2002@gmail.com>
2024-05-03 17:07:15 +0530

    modified:   nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
   devtools: { enabled: false},
   modules: ["@nuxt/ui", "@nuxt/image"],
    ssr: false,
+   target: 'static',
   routeRules:{
     '/':{prerender:true}
   }
